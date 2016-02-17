1a3f7ea655379a906d853f1ec5a0c6fb37dc2fe2
billy1980 <william.ghilardi@gmail.com>
2016-02-17 07:37:43 +0100

    Create connection.h

---
+++ connection.h
@@ -0,0 +1,12 @@
+MYSQL *connection() {
+        MYSQL *conn; /*MySQL Connection */
+
+        /* Attempt to connect to the server */
+        conn = mysql_init(NULL);
+        if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)){
+                perror(mysql_error(conn));
+                return 0;
+        } else {
+                return conn;
+        }
+}
