2ff328a254eea6c569a14a7f00224b0613973c1a
billy1980 <william.ghilardi@gmail.com>
2016-02-18 08:39:30 +0100

    Update connection.h

--- connection.h
+++ connection.h
@@ -1,7 +1,5 @@
 MYSQL *connection() {
-        
         /* Attempt to connect to the server */
-        conn = mysql_init(NULL);
         if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0)){
                 perror(mysql_error(conn));
                 return 0;
